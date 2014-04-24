c077411f46fea5c71fc07010cfe6add48801a69a
vixuowis <yinghuix@qq.com>
2014-04-24 11:50:28 +0800

    Finish trends graph and knobs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,37 +9,36 @@ class HomeController < ApplicationController
   end
 
   def get_trends_data
-	mongo_client = MongoClient.new("localhost", 27017)
-	db = mongo_client.db("ovms_db")
-	coll = db["nvdcve"]
-	
-	# 获取日期列表
-	date_list = []
-	@simple_date_list = []
-	Date.today
-	(-29..0).each do |minus|
-		date_item = Date.today + minus
-		date_list.append(date_item.to_s)
-		@simple_date_list.append(date_item.month.to_s+"."+date_item.day.to_s)
-	end 
+  	mongo_client = MongoClient.new("localhost", 27017)
+  	db = mongo_client.db("ovms_db")
+  	coll = db["nvdcve"]
+  	
+  	# 获取日期列表
+  	date_list = []
+  	@simple_date_list = []
+  	Date.today
+  	(-29..0).each do |minus|
+  		date_item = Date.today + minus
+  		date_list.append(date_item.to_s)
+  		@simple_date_list.append(date_item.month.to_s+"."+date_item.day.to_s)
+  	end 
 
-	# 构造数据
-	layer1 = [] # 新漏洞
-	layer2 = []
-	layer3 = []
-	layer4 = []
-	date_list.each_with_index do |date_item,index|
-		vuln_null_count = coll.find('$and'=>[{'cvss'=>nil},{'modifify'=>date_item}]).count()
-		layer1.append(vuln_null_count)
-		# vuln_low_count = coll.find("this.cvss>=0 && this.cvss<=3.9").count()
-		layer2.append(1)
-		# vuln_mid_count = coll.find("this.cvss>=4.0 && this.cvss<=6.9").count()
-		layer3.append(1)
-		# vuln_high_count = coll.find("this.cvss>=7.0 && this.cvss<=10.0").count()
-		layer4.append(1)
-	end
+  	# 构造数据
+  	layer1 = [] # 新漏洞
+  	layer2 = []
+  	layer3 = []
+  	layer4 = []
 
-	# result = coll.find({"item.Notes.Note"=>"2014-04-25"}).count()
+  	date_list.each_with_index do |date_item,index|
+  		vuln_null_count = coll.find({'cvss'=>nil, 'publish'=>date_item}).count()
+  		layer1.append(vuln_null_count)
+  		vuln_low_count = coll.find({'cvss'=>{'$gte'=>0,'$lte'=>3.9}, 'modify'=>date_item}).count()
+  		layer2.append(vuln_low_count)
+  		vuln_mid_count = coll.find({'cvss'=>{'$gte'=>4.0,'$lte'=>6.9}, 'modify'=>date_item}).count()
+  		layer3.append(vuln_mid_count)
+  		vuln_high_count = coll.find({'cvss'=>{'$gte'=>7.0,'$lte'=>10.0}, 'modify'=>date_item}).count()
+  		layer4.append(vuln_high_count)
+  	end
 
   	data = [
   		layer1,
@@ -47,7 +46,19 @@ class HomeController < ApplicationController
   		layer3,
   		layer4
   	]
-  	
+
   	@trends_data = data.to_s
+
+    # 4个统计盘数据
+    data_panel = [
+      layer1.sum,
+      layer2.sum,
+      layer3.sum,
+      layer4.sum
+    ]
+
+    @panel_data = data_panel
+
   end
+
 end
