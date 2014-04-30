f9d2b167104c6277a188ccd557ee54e353300050
yhxia <yinghuix@qq.com>
2014-04-30 23:29:23 +0800

    Fix homepage sidebar; Know how to use calendar

--- app/assets/javascripts/setup.js
+++ app/assets/javascripts/setup.js
@@ -46,37 +46,62 @@ $(document).ready(function() {
 
     // page is now ready, initialize the calendar...
 
-    $('#calendar').fullCalendar({
-      events: [
-        {
-            title  : 'event1',
-            start  : '2014-04-01'
-        },
-        {
-            title  : 'event2',
-            start  : '2014-04-05',
-            end    : '2014-04-07'
-        },
-        {
-            title  : 'event3',
-            start  : '2014-04-09 12:30:00',
-            allDay : false // will make the time show
-        }
+  $('#calendar').fullCalendar({
+    events: [
+      {
+          title  : 'event1',
+          start  : '2014-04-01'
+      },
+      {
+          title  : 'event2',
+          start  : '2014-04-05',
+          end    : '2014-04-07'
+      },
+      {
+          title  : 'event4',
+          start  : '2014-04-05',
+          end    : '2014-04-07'
+      },
+      {
+          title  : 'event5',
+          start  : '2014-04-05',
+          end    : '2014-04-07'
+      },
+      {
+          title  : 'event6',
+          start  : '2014-04-05',
+          end    : '2014-04-07'
+      },
+      {
+          title  : 'event7',
+          start  : '2014-04-05',
+          end    : '2014-04-07'
+      },
+      {
+          title  : 'event8',
+          start  : '2014-04-05',
+          end    : '2014-04-07'
+      },
+      {
+          title  : 'event3',
+          start  : '2014-04-09 12:30:00',
+          allDay : false // will make the time show
+      }
     ],//'http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic',
-			
-      			eventClick: function(event) {
-      				// opens events in a popup window
-      				//window.open(event.url, 'gcalevent', 'width=700,height=600');
-      				return false;
-      			},
-			
-      			loading: function(bool) {
-      				if (bool) {
-      					$('#loading').show();
-      				}else{
-      					$('#loading').hide();
-      				}
-      			}
-    })
+		
+		eventClick: function(event) {
+			// opens events in a popup window
+			//window.open(event.url, 'gcalevent', 'width=700,height=600');
+			return false;
+		},
+
+		loading: function(bool) {
+			if (bool) {
+				$('#loading').show();
+			}else{
+				$('#loading').hide();
+			}
+		}
+  })
 
 });
